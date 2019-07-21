c128ddb923b06b84f235f7cc85dab9c640ed976e
house60705 <house60705@gmail.com>
2019-07-21 23:05:48 +0800

    using HC to solve backpack problem

---
+++ week2/week2/week2.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <random>
+#include <vector>
+#include <fstream>
+#include <math.h>
+using namespace std;
+
+int Evaluate(vector<int> a, vector<int> weight, vector<int> value, int Bagweight) {
+	int sumWeight = 0, sumValue = 0;
+	for (int i = 0; i < a.size(); i++) {
+
+		if (a[i]) {
+			sumWeight += weight[i];
+			sumValue += value[i];
+		}
+	}
+	if (sumWeight > Bagweight) { return 0; }
+	else { return sumValue; }
+}
+
+vector<int> NeighborNode(vector<int> a, vector<int> weight, vector<int> value, int Bagweight, int random1, int random2)
+{
+	vector<int> b = a;
+	if (random1>random2) {
+		for (int i = random2; i <= random1; i++)
+			b[i] = abs(b[i] - 1);
+	}
+	else {
+		for (int i = random1; i <= random2; i++)
+			b[i] = abs(b[i] - 1);
+	}
+	return b;
+}
+int main(int argc, char* argv[])
+{
+	int  runs = atoi(argv[1]), times = atoi(argv[2]), bits = atoi(argv[3]), round = 1;
+	int  input, num = 1, BagWeight = 165;
+	vector<int> weight, value;
+	ifstream infileWeight("p01_w.txt");
+	while (infileWeight >> input) {
+		weight.push_back(input);
+		num++;
+	}
+	ifstream infileValue("p01_v.txt");
+	while (infileValue >> input) {
+		value.push_back(input);
+	}
+	random_device rd;
+	mt19937 generator(rd());
+	uniform_int_distribution<> unif(0, 1);
+	uniform_real_distribution<> unifr(0, (bits - 1));
+	vector<int> BestSolution(bits, 0);
+	int BestSolutionNum = 0;
+	/*for (auto i = NowSolution.begin(); i != NowSolution.end(); ++i) {
+	cout << *i;
+	}*/
+	while (runs--)
+	{
+		cout << "round : " << round << endl;
+		times = atoi(argv[2]);
+		vector<int> NowSolution(bits, 0);
+		vector<int> NeightborSolution(bits, 0);
+		vector<int> RoundBestSolution(bits, 0);
+		int NowSolutionNum, RoundBestSolutionNum = 0, NeightborSolutionNum = 0;
+		for (auto i = NowSolution.begin(); i != NowSolution.end(); ++i) {
+			*i = unif(generator);
+		}
+		if (Evaluate(NowSolution, weight, value, BagWeight) > RoundBestSolutionNum)
+		{
+			RoundBestSolutionNum = Evaluate(NowSolution, weight, value, BagWeight);
+			RoundBestSolution = NowSolution;
+		}
+
+		while (times--)
+		{
+			NeightborSolution = NeighborNode(NowSolution, weight, value, BagWeight, unifr(generator), unifr(generator));
+			NeightborSolutionNum = Evaluate(NeightborSolution, weight, value, BagWeight);
+			NowSolutionNum = Evaluate(NowSolution, weight, value, BagWeight);
+
+			if (NeightborSolutionNum > NowSolutionNum) {
+				RoundBestSolutionNum = NeightborSolutionNum;
+				RoundBestSolution = NeightborSolution;
+				NowSolution = NeightborSolution;
+				NowSolutionNum = NeightborSolutionNum;
+			}
+		}
+		if (RoundBestSolutionNum > BestSolutionNum)
+		{
+			BestSolutionNum = RoundBestSolutionNum;
+			BestSolution = RoundBestSolution;
+		}
+		round++;
+	}
+	cout << "The best solution is : ";
+	for (auto i = BestSolution.begin(); i != BestSolution.end(); i++) { cout << *i; }
+	cout << " , the number of 1 is	: " << BestSolutionNum << endl;
+}
