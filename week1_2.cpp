42801ef925f57af784a322db83fa1c6e952ffcb8
house60705 <house60705@gmail.com>
2019-07-22 21:04:45 +0800

    modified method

--- week1_2.cpp
+++ week1_2.cpp
@@ -4,52 +4,17 @@
 
 using namespace std;
 
-int numOf1(vector<int> a) {
+int Evaluate(vector<int> a) {
 	int sum = 0;
 	for (auto i = a.begin(); i != a.end(); ++i) {
 		sum += *i;
 	}
 	return sum;
 }
-/*
-vector<int> BinaryAdd(const vector<int>& a, int temp) {
-	vector<int> b(temp, 0);
-	b.back() = 1;
-	vector<int> c(temp);
-	int AddCarry = 0;
-
-	for (int i = temp - 1; i >= 0; i--) {
-		c[i] = ((a[i] ^ b[i]) ^ AddCarry);
-		AddCarry = ((a[i] & b[i]) | (a[i] & AddCarry)) | (b[i] & AddCarry);
-	}
-
-	return c;
-}
-
-vector<int> BinarySub(const vector<int>& a, int temp) {
-	vector<int> b(temp, 1);
-	vector<int> c(temp);
-	int AddCarry = 0;
-
-	for (int i = temp - 1; i >= 0; i--) {
-		c[i] = ((a[i] ^ b[i]) ^ AddCarry);
-		AddCarry = ((a[i] & b[i]) | (a[i] & AddCarry)) | (b[i] & AddCarry);
-	}
-	return c;
-}
-*/
-vector<int> NeighborNode(vector<int>& a, int random)
+vector<int> NeighborNode(vector<int> a, int random)
 {
-	vector<int> b = a;
-
-	if (!b[random]) { b[random] = 1; }
-	else { b[random] = 0; }
-	if (numOf1(a) >= numOf1(b)) { return a; }
-	return b;
-	/*vector<int> nexts = BinaryAdd(a, a.size());
-	vector<int> prev = BinarySub(a, a.size());
-	if (numOf1(next) < numOf1(prev)) { a = prev; }
-	else { a = next; }*/
+	a[random] = abs(a[random] - 1);
+	return a;
 }
 int main(int argc, char** argv)
 {
@@ -57,38 +22,40 @@ int main(int argc, char** argv)
 	random_device rd;
 	mt19937_64 generator(rd());
 	uniform_int_distribution<> unif(0, 1);
-	uniform_real_distribution<> unifr(0, 99);
-	vector<int> BestSolution(100, 0);
+	uniform_real_distribution<> unifr(0, bits - 1);
+	vector<int> BestSolution(bits, 0);
 	int BestSolutionNum = 0;
 	/*for (auto i = NowSolution.begin(); i != NowSolution.end(); ++i) {
-		cout << *i;
+	cout << *i;
 	}*/
 	while (runs--)
 	{
 		times = atoi(argv[2]);
 		vector<int> NowSolution(bits, 0);
 		vector<int> RoundBestSolution(bits, 0);
-		int NowSolutionNum, RoundBestSolutionNum = 0;
+		vector<int> NeightborSolution(bits, 0);
+		int NowSolutionNum, RoundBestSolutionNum = 0, NeightborSolutionNum = 0;
 		cout << "round : " << round << endl;
 		for (auto i = NowSolution.begin(); i != NowSolution.end(); ++i) {
 			*i = unif(generator);
 		}
-		if (numOf1(NowSolution) > RoundBestSolutionNum)
+		if (Evaluate(NowSolution) > RoundBestSolutionNum)
 		{
-			RoundBestSolutionNum = numOf1(NowSolution);
+			RoundBestSolutionNum = Evaluate(NowSolution);
 			RoundBestSolution = NowSolution;
 		}
 
 		while (times--)
 		{
-			NowSolution = NeighborNode(NowSolution, unifr(generator));
-			NowSolutionNum = numOf1(NowSolution);
-			RoundBestSolutionNum = NowSolutionNum;
-			RoundBestSolution = NowSolution;
-			/*for (auto i = NowSolution.begin(); i != NowSolution.end(); ++i) {
-				cout << *i;
+			NeightborSolution = NeighborNode(NowSolution, unifr(generator));
+			NeightborSolutionNum = Evaluate(NeightborSolution);
+			NowSolutionNum = Evaluate(NowSolution);
+			if (NeightborSolutionNum > NowSolutionNum) {
+				RoundBestSolutionNum = NeightborSolutionNum;
+				RoundBestSolution = NeightborSolution;
+				NowSolution = NeightborSolution;
+				NowSolutionNum = NeightborSolutionNum;
 			}
-			cout << endl;*/
 		}
 		if (RoundBestSolutionNum > BestSolutionNum)
 		{
@@ -100,4 +67,5 @@ int main(int argc, char** argv)
 	cout << "The best solution is : ";
 	for (auto i = BestSolution.begin(); i != BestSolution.end(); i++) { cout << *i; }
 	cout << " , the number of 1 is : " << BestSolutionNum << endl;
+	system("pause");
 }
